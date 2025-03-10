f2d77807731b14e638857518276ae1800a8f090c
obinnachima04 <obinnafnn73@gmail.com>
2025-03-10 15:35:09 +0100

    learning python syntax

---
+++ code.py
@@ -0,0 +1,40 @@
+#This is a list of some python method syntax and their functions.
+#A method is an action that python can perform on a piece of data
+
+.title()
+This converts the first letters in a string value to uppercase letters.
+example:
+name = "obinna chima"
+print(name)
+print(name.title())
+
+
+.upper()
+This converts all the letters in a variable value to uppercase.
+example:
+name = "obinna chima"
+print(name)
+print(name.upper())
+
+.lower()
+This converts all the letters in a variable value to lowercase
+example:
+name = "OBinna CHIMa"
+print(name)
+print(name.lower())
+
++
+This is used to combine two separate string values. It is known as concatenation.
+example:
+first_name = "obinna"
+last_name = "chima"
+full_name = first_name + " " + last_name
+print(full_name)
+
+f"{} {}"
+This is also used to concatenate strings. This function is called formatted string
+example:
+first_name = "obinna"
+last_name = "chima"
+full_name = f"{first_name} {last_name}"
+print(full_name)
