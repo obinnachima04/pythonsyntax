a5249215304dec650a38884afcdf542da6dd40eb
obinnachima04 <obinnafnn73@gmail.com>
2025-03-10 16:02:19 +0100

    added new syntax at no 5

--- code.py
+++ code.py
@@ -1,40 +1,54 @@
 #This is a list of some python method syntax and their functions.
 #A method is an action that python can perform on a piece of data
 
-.title()
-This converts the first letters in a string value to uppercase letters.
+(1) .title()
+#This converts the first letters in a string value to uppercase letters.
 example:
 name = "obinna chima"
 print(name)
 print(name.title())
 
 
-.upper()
-This converts all the letters in a variable value to uppercase.
+(2) .upper()
+#This converts all the letters in a variable value to uppercase.
 example:
 name = "obinna chima"
 print(name)
 print(name.upper())
 
-.lower()
-This converts all the letters in a variable value to lowercase
+(3) .lower()
+#This converts all the letters in a variable value to lowercase
 example:
 name = "OBinna CHIMa"
 print(name)
 print(name.lower())
 
-+
-This is used to combine two separate string values. It is known as concatenation.
-example:
-first_name = "obinna"
-last_name = "chima"
-full_name = first_name + " " + last_name
-print(full_name)
 
-f"{} {}"
-This is also used to concatenate strings. This function is called formatted string
+(4) f"{} {}"
+#This is used to concatenate strings. This function is called formatted string
 example:
 first_name = "obinna"
 last_name = "chima"
 full_name = f"{first_name} {last_name}"
 print(full_name)
+
+
+(5) #removing whitestrips
+    #whitestrrips are blank spaces before or after a value
+example:
+(a)  .rstrip()
+#this removes any blank space from the right side of a value
+name = "obinna  "
+print(name)
+print(name.rstrip())
+
+(b) .lstrip()
+#this removes any blank space from the left side of a value.
+name = "   chima"
+print(name)
+print(name.lstrip())
+
+(c) .strip
+name = "   chris   "
+print(name)
+print(name)
